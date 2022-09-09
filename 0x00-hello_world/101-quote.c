501e24930d192352aee2214a2201749a06c71830
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-08 19:16:24 -0700

    Unix

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	write("and that piece of is useful\" - Dora Korpr, 2015-10-19\n" ,59,1,stderr);
+       return (1)	
+}
