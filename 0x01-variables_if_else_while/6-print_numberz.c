a46e3ff75a6eeb40a87af105e539a982bdcc0165
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-09 18:04:18 -0700

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,15 @@
+#include <stdio>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int y = 0;
+
+	for (; y < 10; y++)
+		putchar('0' + y);
+	putchar('\n');
+		return (0);
+}
