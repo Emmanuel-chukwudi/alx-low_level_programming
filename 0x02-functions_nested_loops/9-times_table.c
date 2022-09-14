b88bfa93fcb3d0979d14bbfd24186c61c338d3e3
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-14 00:51:49 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,40 @@
+#include "main"
+
+/**
+ * times_table - prints 9 times table, starting with 0
+ *
+ * r: for rows
+ * c: for colums
+ * Return: times table
+ */
+void times_table(void)
+{
+	int r, c, d;
+
+	for (r = 0; r <= 9; r++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (c = 0; c <= 9; r++)
+		{
+			d = (r * c);
+			if ((d / 10) > 0)
+			{
+				_putchar((d / 10) + '0')
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 10) + '0');
+
+			if (c < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
