8e231c5050238b042f13d9024f7d187e7be3f4f5
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-15 02:41:50 -0700

    0-main.c

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - This function prints positive and negative numbers
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
