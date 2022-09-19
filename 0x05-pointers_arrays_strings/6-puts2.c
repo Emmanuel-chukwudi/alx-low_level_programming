3a678404c0df0f31891e436f780db53a3923c81a
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-19 14:50:30 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,14 +11,12 @@ void puts2(char *str)
 {
 	int a = 0;
 
-	while (str[a] != '\0')
+	while ((str[a] != '\0') && (a % 2 == 0))
 	{
-		if (a % 2 == 0)
-		{
-			_putchar(str[a]);
-		}
-		a++;
+		_putchar(str[a]);
 	}
+	a++;
+
 	_putchar('\n');
 }
 
