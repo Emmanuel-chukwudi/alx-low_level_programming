234356bd2f31ca253d5661f226961dc0f1df5cb3
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-19 14:32:23 -0700

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * puts2 - prints the even index of a string
+ * @str: string
+ *
+ * Return: Always 0 (Success)
+ */
+
+void puts2(char *str)
+{
+	int a;
+
+	while ((str[a] != '\0') && (a % 2 == 0))
+	{
+		_putchar(str[a]);
+		a++;
+	}
+}
