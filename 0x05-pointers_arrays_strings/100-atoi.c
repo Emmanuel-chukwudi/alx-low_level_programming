8faf8364007ba8d767aea91760a52e4d18a4cddb
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-19 16:32:57 -0700

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Random password generator
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int a, b, d, s;
+	char c[] = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char p[58];
+
+	srand(time(NULL));
+	while (s != 2772)
+	{
+		a = d = s = 0;
+		while ((2772 - 122) > s)
+		{
+			b = rand() % 62;
+			p[a] = c[b];
+			s += c[b];
+			a++;
+		}
+		while (c[d])
+		{
+			if (c[d] == (2772 - s))
+			{
+				p[a] = c[d];
+				s += c[d];
+				a++;
+				break;
+			}
+			d++;
+		}
+	}
+	p[a] = '\0';
+	printf("%s", p);
+	return (0);
+}
