468e816399d418ef6ea8e7165d8be34bcee845f7
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-22 09:40:58 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - converts lower case letters to upper case
+ * @c: string to convert
+ * Return: upper case string
+ */
+
+char *string_toupper(char *c)
+{
+	int b = 0;
+
+	for (b = 0; c[b] != '\0'; b++)
+	{
+		if (c[b] >= 'a' && c[b] <= 'z')
+		{
+			c[b] = c[b] - ('a' + 'z');
+		}
+	}
+	return (c);
+}
