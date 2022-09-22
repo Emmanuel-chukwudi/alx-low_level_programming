c407624abb1d6862318cd22248e031c2f5e4a23a
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-22 13:32:23 -0700

    100-rot13.c

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -2,10 +2,31 @@
 
 /**
  * rot13 - encodes a string using rot13
- * @c - string
+ * @c: string
  * Return: c
  */
 
 char *rot13(char *c)
 {
+	char p[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char q[] = "nopqrstuvwxyzabcdefghijklmnopQRSTUVWXYZABCDEFGHIJKLM";
 
+	int a;
+	int b;
+
+	for (b = 0; c[b] != '\0'; b++)
+	{
+		a = 0;
+		while ((p[a] != '\0') && (c[b] != p[a]))
+		{
+			a++;
+		}
+
+		if (c[b] == p[a])
+		{
+			c[b] = q[a];
+		}
+	}
+
+	return (c);
+}
