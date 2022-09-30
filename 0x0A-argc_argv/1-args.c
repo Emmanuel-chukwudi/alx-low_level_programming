09a17539d3a0931aea29aabde80228f8c35b7667
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-09-30 09:45:38 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - a program that prints the number of arguements passed into it
+ * @argc: argument counter
+ * @argv: argument vertor, an array
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, __attribute__ ((unused)) char *argv[])
+{
+	printf("%d\n", argc - 1)
+
+		return (0);
+}
+
