abf6c148fa6ca853d7ffa0cc067d88b17f2b9254
Emmanuel <Chukwudiemmanueljnr@gmail.com>
2022-10-10 06:06:11 -0700

    main.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - creating a new struct dog
+ *
+ * Return: Always 0 (Success)
+ */
+
+struct dog
+{
+	char name;
+	float age;
+	char owner;
+};
+
+int main(void)
+{
+	struct dog my_dog;
+
+	return (0);
+}
--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,9 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-#include <stdlib.h>
-#include <stdio.h>
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 
 void print_dog(struct dog *d);
